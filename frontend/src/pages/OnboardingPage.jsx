173550a2264129b77baf4fe9d360c4c7068e3c3a
NoopurY <noopuratulyawalkar@gmail.com>
2025-06-25 11:23:06 +0530

    Update OnboardingPage.jsx

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -38,8 +38,8 @@ const OnboardingPage = () => {
   };
 
   const handleRandomAvatar = () => {
-    const idx = Math.floor(Math.random() * 100) + 1; // 1-100 included
-    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const seed = Math.floor(Math.random() * 100000);
+const randomAvatar = `https://api.dicebear.com/7.x/bottts/svg?seed=${seed}`;
 
     setFormState({ ...formState, profilePic: randomAvatar });
     toast.success("Random profile picture generated!");
